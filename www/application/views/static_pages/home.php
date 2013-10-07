28dfa008a009c7a794001eecfe6f6a43997c4865
Alain <hello@alainbarthelemy.com>
2013-10-07 12:34:26 +0200

    the mind

--- www/application/views/static_pages/home.php
+++ www/application/views/static_pages/home.php
@@ -1,5 +1,5 @@
 <h1 style="color:red;">TO BE REMOVED //THIS IS THE HOME PAGE // TO BE REMOVED</h1>
-
+the mind is a terrible thing to waste
 <!--<a href="/index.php/about">about</a>
 <a href="/index.php/projects/prjkts">projects list</a>
 <a href="/index.php/projects/prjkts/projects_thumb_list">projects thumb list</a>-->
